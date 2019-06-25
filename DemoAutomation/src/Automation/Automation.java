9757a7d79f419e41e1e665746086f351bbe7faf3
arnab mondal <arnab.mondal@coxautoinc.com>
2019-06-25 15:35:02 +0530

    Arnab:This is my first commit

---
+++ DemoAutomation/src/Automation/Automation.java
@@ -0,0 +1,11 @@
+package Automation;
+
+public class Automation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Code from my System");
+	}
+
+}
