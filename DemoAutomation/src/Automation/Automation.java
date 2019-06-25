64871a4f9aff19c2a12ee7cb1fe4122de0360665
arnabmondal1001 <52192693+arnabmondal1001@users.noreply.github.com>
2019-06-25 15:42:44 +0530

    Update Automation.java
    This is changed from github itself

--- DemoAutomation/src/Automation/Automation.java
+++ DemoAutomation/src/Automation/Automation.java
@@ -5,7 +5,7 @@ public class Automation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Code from my System");
+		System.out.println("Code from my GITHUB");
 	}
 
 }
